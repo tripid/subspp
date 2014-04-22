ff53b94a9344e579d8113dd06d6ead05fc9b6f38
Tristan Peralta <tristanperalta@gmail.com>
2014-04-22 15:32:27 +0800

    Subscribe link - read site from config if site from params are not specified

--- lib/subspp/subscribe_link.rb
+++ lib/subspp/subscribe_link.rb
@@ -2,14 +2,19 @@ module Subspp
   class SubscribeLink
     include Base
 
-    attr_accessor :customer_id, :plan_id, :screen_name, :site
+    attr_accessor :customer_id, :plan_id, :screen_name, :configuration
 
     def initialize(options)
       set_instance_variable_from_options(options)
+      @configuration = Subspp.configuration
+    end
+
+    def site
+      @site || @configuration.site
     end
 
     def to_url
-      [ Subspp.configuration.host, site, 'subscribers', customer_id,
+      [ configuration.host, site, 'subscribers', customer_id,
         'subscribe', plan_id, screen_name  ].join('/')
     end
   end
--- spec/subscriber_link_spec.rb
+++ spec/subscriber_link_spec.rb
@@ -1,20 +1,47 @@
 require 'spec_helper'
 
 describe Subspp::SubscribeLink do
-  params = { site: 'test-site',
-             plan_id: 55,
-             customer_id: 1,
-             screen_name: 'juan-dela-cruz' }
+  context 'attributes' do
+    params = { site: 'test-site',
+               plan_id: 55,
+               customer_id: 1,
+               screen_name: 'juan-dela-cruz' }
 
-  subject { Subspp::SubscribeLink.new(params) }
+    subject { Subspp::SubscribeLink.new(params) }
 
-  params.each do |key, value|
-    specify key do
-      expect(subject.send(key)).to eq params[key]
+    params.each do |key, value|
+      specify key do
+        expect(subject.send(key)).to eq params[key]
+      end
     end
   end
 
-  specify :to_url do
-    expect(subject.to_url).to eq "https://subs.pinpayments.com/test-site/subscribers/1/subscribe/55/juan-dela-cruz"
+  describe '#site' do
+    let(:params) { { plan_id: 55,
+                     customer_id: 1,
+                     screen_name: 'Juan dela Cruz' } }
+
+
+    it 'returns site from config' do
+      Subspp.configure do |config|
+        config.site = 'config-site'
+      end
+      subscribe_link = Subspp::SubscribeLink.new(params)
+      expect(subscribe_link.site).to eq 'config-site'
+    end
+
+    it 'returns site from params' do
+      params[:site] = 'this-site'
+      subscribe_link = Subspp::SubscribeLink.new(params)
+      expect(subscribe_link.site).to eq 'this-site'
+    end
+  end
+
+  specify '#to_url' do
+    subscribe_link = Subspp::SubscribeLink.new( site: 'test-site',
+                                                plan_id: 55,
+                                                customer_id: 1,
+                                                screen_name: 'juan-dela-cruz' )
+    expect(subscribe_link.to_url).to eq "https://subs.pinpayments.com/test-site/subscribers/1/subscribe/55/juan-dela-cruz"
   end
 end
